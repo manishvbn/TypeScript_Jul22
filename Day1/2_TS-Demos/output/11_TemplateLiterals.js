62e9cab6dd1a8d327b9b380c50c3585f4fbc4e6a
manishvbn <manishvbn@gmail.com>
2022-07-09 15:40:36 +0530

    TemplateLiterals

---
+++ Day1/2_TS-Demos/output/11_TemplateLiterals.js
@@ -0,0 +1,7 @@
+"use strict";
+var fname = "Manish";
+var lname = "Sharma";
+var message1 = "Hello, " + fname + " " + lname;
+console.log(message1);
+var message2 = "Hello, " + fname + " " + lname;
+console.log(message2);
