6167bf732c65e3c6b8e9af78a588f818c5a60cf1
manishvbn <manishvbn@gmail.com>
2022-07-10 12:21:32 +0530

    Generics

---
+++ Day2/1_TSDemos/output/7_Generics.js
@@ -0,0 +1,8 @@
+"use strict";
+function getPropValue(obj, key) {
+    return obj[key];
+}
+var person = { id: 1, name: "Manish", city: "Pune" };
+console.log(getPropValue(person, "id"));
+console.log(getPropValue(person, "name"));
+console.log(getPropValue(person, "city"));
