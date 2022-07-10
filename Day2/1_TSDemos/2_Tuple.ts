8b1597598b49e70a468d2ba782a509c65e7ef136
manishvbn <manishvbn@gmail.com>
2022-07-10 09:29:08 +0530

    Tuples

---
+++ Day2/1_TSDemos/2_Tuple.ts
@@ -0,0 +1,28 @@
+// TypeGuard Array
+
+var dataArr1: (number | string)[];
+dataArr1 = [1, "Manish"];
+dataArr1 = ["Manish", 1];
+dataArr1 = ["Manish", "Pune"];
+dataArr1 = [1, 2, 3, 4, 5];
+dataArr1 = ["Manish", 1, "Pune", 411021];
+
+// Tuple - Stores a fixed collection of values of same or varied types, maintaining the sequence
+let dataRow: [number, string];
+dataRow = [1, "Manish"];
+// dataRow = ["Manish", 1];
+// dataRow = ["Manish", "Pune"];
+// dataRow = [1, 2];
+// dataRow = [1, 2, 3, 4, 5];
+// dataRow = [1, "Manish", 411021, "Pune"];
+
+// function insert(data: (number | string)[]) {
+//     // Code to insert the data as row of a table
+// }
+
+function insert(data: [number, string]) {
+    // Code to insert the data as row of a table
+}
+
+insert(["Manish", 1, "Pune", 411021]);
+insert([1, "Manish"]);
---
+++ Day2/1_TSDemos/output/2_Tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+var dataArr1;
+dataArr1 = [1, "Manish"];
+dataArr1 = ["Manish", 1];
+dataArr1 = ["Manish", "Pune"];
+dataArr1 = [1, 2, 3, 4, 5];
+dataArr1 = ["Manish", 1, "Pune", 411021];
