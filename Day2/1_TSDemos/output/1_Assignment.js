eb82481440337cb467dd1720cab4119de1a6f0ee
manishvbn <manishvbn@gmail.com>
2022-07-10 09:19:30 +0530

    Assignment

--- Day2/1_TSDemos/output/1_Assignment.js
+++ Day2/1_TSDemos/output/1_Assignment.js
@@ -1,5 +1,9 @@
 "use strict";
 function Reverse(strOrArr) {
+    if (typeof strOrArr === "string")
+        return strOrArr.split("").reverse();
+    else
+        return strOrArr.reverse();
 }
 console.log(Reverse("Manish"));
 console.log(Reverse(["PQR", "XYZ", "ABC"]));
